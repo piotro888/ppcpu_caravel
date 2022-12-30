971037f54bebba38178a2b00a708e17a707a4932
Piotro <piotro888@wp.pl>
2022-12-30 14:53:12 +0100

    Simple cw boot test

---
+++ verilog/dv/boot_cw/boot_cw.c
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2020 Efabless Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// This include is relative to $CARAVEL_PATH (see Makefile)
+#include <defs.h>
+#include <stub.c>
+
+
+void main()
+{
+	// reg_mprj_io_8 =  GPIO_MODE_USER_STD_OUTPUT;
+	// reg_mprj_io_9 =  GPIO_MODE_USER_STD_OUTPUT;
+	// reg_mprj_io_37 =  GPIO_MODE_USER_STD_OUTPUT;
+
+    // while (reg_mprj_xfer == 1);
+	// for(;;);
+}
+
